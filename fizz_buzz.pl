56e24bc39caff1c954f519efa88bf06d4f441a6d
Cory Spitzer <coryspitzer@gmail.com>
2016-03-29 13:56:46 -0400

    Fizzbuzz

--- fizz_buzz.pl
+++ fizz_buzz.pl
@@ -3,7 +3,17 @@
 use strict;
 use warnings;
 
-for (my $i = 0; $i <= 30; $i++)
-{
-  print("$i\n");
+for (my $i = 0; $i <= 30; $i++) {
+  if ($i % 15 == 0) {
+    print("FizzBuzz\n");
+
+  } elsif ($i % 5 == 0) {
+    print("Fizz\n");
+
+  } elsif ($i % 3 == 0) {
+    print("Buzz\n");
+
+  } else {
+    print("$i\n");
+  }
 }
